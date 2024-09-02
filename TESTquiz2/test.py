63786ee9dea5f67374b1b82a2f47341ebd3b6628
cmosnato <cmoscmark@gmail.com>
2024-09-02 22:28:58 +0700

    fix Quiz 2 รอบ 2

--- TESTquiz2/test.py
+++ TESTquiz2/test.py
@@ -4,7 +4,7 @@ name_rate = [None,"Standard","Premium","Rare"]
 print("################Menu################\n ####  1.Standard 2.Premium 3.Rare  ####\n####################################")
 for index, value in enumerate(rate):
     if index == 0:
-        continue  # ข้ามตำแหน่งที่ 0
+        continue  
     print(f"Rate {index} : {value} Bath ", end="  ")
 print(" ")
 selectRate = int(input("Welcome CPE_SUCK chose your rate : [1-3] : "))
@@ -24,45 +24,52 @@ change = -pay
 if change == 0 :
     print("no change")
 print(f"change {change}")
-amount1 = 0
-amount2 = 0
-amount3 = 0
-amount4 = 0 
-amount5 = 0
+amount = []
+posi = -1
+# amount1 = 0
+# amount2 = 0
+# amount3 = 0
+# amount4 = 0 
+# amount5 = 0
 while change > 0:
     if change >= 1000:
         print("##########change money###########")
-        amount1 = change//1000
+        posi = posi +1
+        amount.append(change//1000)
         change = change%1000
-        if amount1 >= 1:
-            print(f"change : 1000  Amount : {amount1}")
+        if amount[posi] >= 1:
+            print(f"change : 1000  Amount : {amount[posi]}")
     elif change < 1000 and change >= 500:
         print("##########change money###########")
-        amount2 = change//500
+        posi = posi +1
+        amount.append(change//500)
         change = change%500
-        if amount2 >= 1:
-            print(f"change : 500  Amount : {amount2}")
+        if amount[posi] >= 1:
+            print(f"change : 500  Amount : {amount[posi]}")
         
     elif change < 500 and change >=100:
         print("##########change money###########")
-        amount3 = change//100
+        posi = posi +1
+        amount.append(change//100)
         change = change%100
-        if amount3 >= 1:
-            print(f"change : 100  Amount : {amount3}")
+        if amount[posi] >= 1:
+            print(f"change : 100  Amount : {amount[posi]}")
         
     elif change <100  and change >=50 :
         print("##########change money###########")
-        amount4 = change//50
+        posi = posi +1
+        amount.append(change//50)
         change = change%50
-        if amount4 >= 1:
-            print(f"change : 50  Amount : {amount4}")
+        if amount[posi] >= 1:
+            print(f"change : 50  Amount : {amount[posi]}")
 
     elif change <50  and change >=20 :
         print("##########change money###########")
-        amount5 = change//20
+        posi = posi +1
+        amount.append(change//20)
         change = change%20
-        if amount5 >= 1:
-            print(f"change : 20  Amount : {amount5}") 
+        if amount[posi] >= 1:
+            print(f"change : 20  Amount : {amount[posi]}") 
 
     elif change == 10 :
         print("##########change money###########")
